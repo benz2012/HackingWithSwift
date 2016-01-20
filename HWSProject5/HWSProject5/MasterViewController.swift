34ba584858193934fdd0aa4b136cddcaac52a67a
Ben Zenker <benz2012@verizon.net>
2016-01-20 12:26:46 -0500

    Uploading completed projects

---
+++ HWSProject5/HWSProject5/MasterViewController.swift
@@ -0,0 +1,72 @@
+//
+//  MasterViewController.swift
+//  HWSProject5
+//
+//  Created by Ben on 12/28/15.
+//  Copyright © 2015 Ben Zenker. All rights reserved.
+//
+
+import UIKit
+import GameplayKit
+
+class MasterViewController: UITableViewController {
+
+    //var detailViewController: DetailViewController? = nil
+    var objects = [String]()
+    var allWords = [String]()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let startWordsPath = NSBundle.mainBundle().pathForResource("benstart", ofType: "txt") {
+            if let startWords = try? String(contentsOfFile: startWordsPath, usedEncoding: nil) {
+                allWords = startWords.componentsSeparatedByString("\n")
+            }
+        } else {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
+        super.viewWillAppear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func startGame() {
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(allWords) as! [String]
+        title = allWords[0]
+        objects.removeAll(keepCapacity: true)
+        tableView.reloadData()
+    }
+
+    // MARK: - Segues
+
+    // MARK: - Table View
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object
+        return cell
+    }
+
+
+}
+
